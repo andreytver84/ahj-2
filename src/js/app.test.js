9be25577a0f4c2f7ca0369ad479baa0a30f1dedf
Андрей Увиков <uvikmedium@mail.ru>
2022-12-26 23:26:29 +0300

    problem with import func from app to test

---
+++ src/js/app.test.js
@@ -0,0 +1,14 @@
+//import demo from "./app.js";
+function demo(value) {
+  return `Demo: ${value}`;
+}
+
+describe("Пример теста", () => {
+  test.each([
+    { str: "Hello!", expected: "Demo: Hello!" },
+    { str: "", expected: "Demo: " },
+    { str: 100, expected: "Demo: 100" },
+  ])("demo($str)", ({ str, expected }) => {
+    expect(demo(str)).toBe(expected);
+  });
+});
