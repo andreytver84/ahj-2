ca7ca35d66da3930026c261e39a5d2973a1dd2ef
Андрей Увиков <uvikmedium@mail.ru>
2022-12-26 21:05:06 +0300

    add Babel & polifils

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     mode: 'development',
-    entry: './index.js',
+    entry: ["@babel/polyfill", "./index.js"],
     output: {
-        filename: '[contenthash].js',
+        filename: '[name][hash].js',
         path: path.resolve(__dirname, 'dist')
     },
     devServer: {
@@ -21,11 +21,11 @@ module.exports = {
         }),
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
-            filename: '[contenthash].css'
+            filename: '[name][hash].css'
         }),
         new CopyPlugin({
             patterns: [
-              { from: path.resolve(__dirname, "src/img"), to: path.resolve(__dirname, "dist/img") }
+              { from: path.resolve(__dirname, 'src/img'), to: path.resolve(__dirname, 'dist/img') }
             ],
           })
     ],
@@ -33,12 +33,22 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader"]
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
             },
             {
                 test: /\.(png|jpg|webp|svg|gif)$/,
                 use: ['file-loader']
-            }
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+              }
         ]
     }
 }
