a9906d16817fa843b3be70697df81b2dc935b41a
Андрей Увиков <uvikmedium@mail.ru>
2022-12-26 20:26:09 +0300

    add plugins & loaders for html css img

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
-const path = require('path')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -10,17 +12,32 @@ module.exports = {
         filename: '[contenthash].js',
         path: path.resolve(__dirname, 'dist')
     },
+    devServer: {
+        port: 4000
+    },
     plugins: [
         new HTMLWebpackPlugin({
             template: './index.html'
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: '[contenthash].css'
+        }),
+        new CopyPlugin({
+            patterns: [
+              { from: path.resolve(__dirname, "src/img"), to: path.resolve(__dirname, "dist/img") }
+            ],
+          })
     ],
     module: {
         rules: [
             {
                 test: /\.css$/,
-                use: ['style-loader','css-loader']
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            },
+            {
+                test: /\.(png|jpg|webp|svg|gif)$/,
+                use: ['file-loader']
             }
         ]
     }
