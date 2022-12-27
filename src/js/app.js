bd561deb6fc7687c785700c408b2c7a29f9f6a7a
Андрей Увиков <uvikmedium@mail.ru>
2022-12-27 16:55:25 +0300

    add script, problem with bg-images

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,29 @@
-// TODO: write code here
-
-// comment this to pass build
-const unusedVariable = "variable";
-
 // for demonstration purpose only
 export default function demo(value) {
   return `Demo: ${value}`;
 }
 
-console.log("app.js included");
+class GameGoblin {
+  constructor(element) {
+    this._element = element;
+  }
+  addGoblin() {
+    const gameTds = this._element.querySelectorAll(".game-td");
+    let number = Math.floor(Math.random() * gameTds.length);
+    const goblin = document.createElement("div");
+    goblin.classList.add("goblin");
+    gameTds[number].append(goblin);
+    this.removeGoblin(goblin);
+  }
+  removeGoblin(td) {
+    setTimeout(() => {
+      td.remove();
+    }, 1000);
+  }
+}
+
+const goblinGame = new GameGoblin(document.querySelector(".game-block"));
+
+setInterval(() => {
+  goblinGame.addGoblin();
+}, 1000);
