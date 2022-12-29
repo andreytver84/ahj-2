92b65649913601a6f06ba073dd47da55f5979165
Андрей Увиков <uvikmedium@mail.ru>
2022-12-29 20:53:26 +0300

    change setInterval & add fix from double td

--- src/js/app.js
+++ src/js/app.js
@@ -2,23 +2,34 @@ class GameGoblin {
   constructor(element) {
     this._element = element;
   }
+  getNumb(n) {
+    return Math.floor(Math.random() * n);
+  }
   addGoblin() {
     const gameTds = this._element.querySelectorAll(".game-td");
-    let number = Math.floor(Math.random() * gameTds.length);
+    let number = this.getNumb(gameTds.length);
+    if (gameTds[number].classList.contains("active")) {
+      console.log("повтор");  
+      let newNumber = this.getNumb(gameTds.length);
+      number = number == newNumber ? this.getNumb(gameTds.length) : newNumber;           
+    }
+    gameTds.forEach(item => item.classList.remove("active"));
     const goblin = document.createElement("div");
-    goblin.classList.add("goblin");
-    gameTds[number].append(goblin);
+    goblin.classList.add("goblin"); 
+    console.log(number);  
+    gameTds[number].classList.add("active");
+    console.log(Number(gameTds[number].getAttribute('data-id')) - 1);
+    gameTds[number].append(goblin);    
     this.removeGoblin(goblin);
   }
   removeGoblin(td) {
     setTimeout(() => {
       td.remove();
+      this.addGoblin();
     }, 1000);
   }
 }
 
 const goblinGame = new GameGoblin(document.querySelector(".game-block"));
 
-setInterval(() => {
-  goblinGame.addGoblin();
-}, 1000);
+goblinGame.addGoblin();
